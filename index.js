be6972e5dfce99aaa238e4f8f821cc74ae8c8f9c
Chloe <cjzelano@gmail.com>
2026-06-11 17:18:48 -0400

    finished assignment

--- index.js
+++ index.js
@@ -6,8 +6,27 @@
  * @returns {boolean} whether the wizard can cast the spell
  */
 function canCastSpell(isSpellPrepared, hasScroll) {
-  // TODO
+  // does the wizard have a spell or scroll?
+  if (typeof isSpellPrepared !== "boolean" || typeof hasScroll !== "boolean") {
+    return "Please input true or false!";
+  }
+  // } else if (isSpellPrepared !== true && hasScroll !== true) {
+  //   return "You have no spell prepared and no scroll, you're doomed!";
+  // } else if (hasScroll === true && isSpellPrepared === true) {
+  //   return "You got the spell and a scroll, choose one and use it now!";
+  // } else if (isSpellPrepared === true) {
+  //   return "Good on you mate, you've got a spell preppared. Use it now!";
+  // } else {
+  //   return "Thankfully, you have a scroll. Use it now!";
+  // }
+  return isSpellPrepared === true || hasScroll === true;
 }
+
+console.log(canCastSpell(true, true)); // true
+console.log(canCastSpell(false, false)); // false
+console.log(canCastSpell(true, false)); // true
+console.log(canCastSpell(false, true)); // true
+console.log(canCastSpell("false", true)); // errpr
 
 /**
  * A creature is hidden from an observer if it is actively hiding
@@ -17,8 +36,18 @@ function canCastSpell(isSpellPrepared, hasScroll) {
  * @returns {boolean} whether the creature is hidden from the observer
  */
 function isHidden(hiding, aware) {
-  // TODO
+  // is the creature hidden from view?
+  if (typeof hiding !== "boolean" || typeof aware !== "boolean") {
+    return "Please input true or false!";
+  }
+  return hiding === true || aware !== true;
 }
+
+console.log(isHidden(true, true)); // true
+console.log(isHidden(false, false)); // true
+console.log(isHidden(true, false)); // true
+console.log(isHidden(false, true)); // false
+console.log(isHidden(true, 1)); // error
 
 /**
  * A strike hits if the attack value is greater than or equal
@@ -28,8 +57,17 @@ function isHidden(hiding, aware) {
  * @returns {boolean} whether the strike hits
  */
 function doesStrikeHit(attack, ac) {
-  // TODO
+  // does the strike land?
+  if (typeof attack !== "number" || typeof ac !== "number") {
+    return "Please enter in a number for attack and the armor class (ac)";
+  }
+  return attack >= ac;
 }
+
+console.log(doesStrikeHit(16, 5)); // true
+console.log(doesStrikeHit(12, 12)); // true
+console.log(doesStrikeHit(5, 12)); // false
+console.log(doesStrikeHit(true, 20)); // error
 
 /**
  * A strike is a critical hit if the attack value is at least
@@ -39,8 +77,16 @@ function doesStrikeHit(attack, ac) {
  * @returns {boolean} whether the strike is a critical hit
  */
 function doesStrikeCrit(attack, ac) {
-  // TODO
+  // is the strike critical?
+  if (typeof attack !== "number" || typeof ac !== "number") {
+    return "Please enter in a number for attack and the armor class (ac)";
+  }
+  return attack > 10 + ac;
 }
+
+console.log(doesStrikeCrit(16, 5)); // true
+console.log(doesStrikeCrit(5, 15)); // false
+console.log(doesStrikeCrit("5", 20)); // error
 
 /**
  * A creature can restore hit points (HP) by healing,
@@ -51,8 +97,25 @@ function doesStrikeCrit(attack, ac) {
  * @returns {number} total hit points after healing
  */
 function heal(maxHp, currentHp, healAmount) {
-  // TODO
+  // total hit points after healing, they cannot be more than the max, but can heal all the way to max and not use remainder.
+  if (
+    typeof maxHp !== "number" ||
+    typeof currentHp !== "number" ||
+    typeof healAmount !== "number"
+  ) {
+    return "Please enter in a number for max hit points, the current hit points and the healed amount";
+  }
+  let healedHp = currentHp + healAmount;
+  if (healedHp > maxHp) {
+    return maxHp;
+  } else {
+    return healedHp;
+  }
 }
+
+console.log(heal(100, 50, 25)); // 75
+console.log(heal(100, 50, 125)); // 100
+console.log(heal("100", 50, 5)); // error
 
 /**
  * When a character uses a skill they have proficiency in,
@@ -71,8 +134,37 @@ function heal(maxHp, currentHp, healAmount) {
  * @returns {number} the character's proficiency bonus
  */
 function getProficiencyBonus(level, rank) {
-  // TODO
+  // give the bonus based on rank & level using addition.
+  if (typeof level !== "number" || typeof rank !== "string") {
+    return "Please enter your level as a number and your rank as a word.";
+  }
+  let bonus = 0;
+  if (rank === "untrained") {
+    bonus = 0; //not adding return to these because I do not want to exit the function!!
+  } else if (rank === "trained") {
+    bonus = 2;
+  } else if (rank === "expert") {
+    bonus = 4;
+  } else if (rank === "master") {
+    bonus = 6;
+  } else if (rank === "legendary") {
+    bonus = 8;
+  } else {
+    return "Invalid rank. Please choose untrained, trained, expert, master, or legendary.";
+  }
+
+  if (rank === "untrained") {
+    return 0;
+  }
+
+  let proficiency = level + bonus;
+  return proficiency;
 }
+
+console.log(getProficiencyBonus(10, "legendary")); // 18
+console.log(getProficiencyBonus(13, "untrained")); // 0
+console.log(getProficiencyBonus(true, "master")); // error
+console.log(getProficiencyBonus(4, "train")); //error
 
 /**
  * A creature can get a bonus to its armor class (AC) by taking cover.
@@ -85,8 +177,34 @@ function getProficiencyBonus(level, rank) {
  * @returns {number} the cover bonus to AC
  */
 function getCoverBonus(behindObstacle, takingCover) {
-  // TODO
+  // cover = bonus to ac
+  // behing obstacle = ac + 2
+  // active cover = ac + 4
+  // no cover = no bonus
+
+  if (typeof behindObstacle !== "boolean" || typeof takingCover !== "boolean") {
+    return "Please enter true or false for both inputs.";
+  }
+
+  let coverBonus = 0;
+  const BEHIND_COVER_BONUS = 2;
+  const ACTIVE_COVER_BONUS = 4;
+
+  if (behindObstacle && takingCover) {
+    coverBonus = ACTIVE_COVER_BONUS;
+  } else if (behindObstacle) {
+    coverBonus = BEHIND_COVER_BONUS;
+  } else if (takingCover) {
+    coverBonus = ACTIVE_COVER_BONUS;
+  }
+  return coverBonus;
 }
+
+console.log(getCoverBonus(true, true)); // 4
+console.log(getCoverBonus(true, false)); // 2
+console.log(getCoverBonus(false, true)); // 4
+console.log(getCoverBonus(false, false)); // 0
+console.log(getCoverBonus(false, "true")); // error
 
 /**
  * A creature's current hit points (HP) is reduced by taking damage.
@@ -101,8 +219,37 @@ function getCoverBonus(behindObstacle, takingCover) {
  * @returns {number} the creature's remaining HP after taking damage
  */
 function getRemainingHp(maxHp, currentHp, damage) {
-  // TODO
+  if (
+    typeof maxHp !== "number" ||
+    typeof currentHp !== "number" ||
+    typeof damage !== "number"
+  ) {
+    return "Please enter the max hp, current hp and damage as a number.";
+  }
+
+  let activeHp = currentHp - damage;
+  let instaDeath = -1;
+  let noHp = 0;
+
+  if (damage >= 2 * maxHp) {
+    return instaDeath;
+  } else if (activeHp <= 0) {
+    return noHp;
+  } else {
+    return activeHp;
+  }
 }
+
+console.log(getRemainingHp(100, 45, 50)); // 0
+console.log(getRemainingHp(100, 45, 200)); // -1
+console.log(getRemainingHp(100, 45, 17)); // 28
+console.log(getRemainingHp(100, "45", 17)); // error
+
+// subtract damage from currentHP ---> NEW currentHP
+// damage >= 2 * maxHp ---> creature dies instanly
+// currentHP = -1 ---> Creature dies instantly ---> Return this
+// currentHp <= 0 ---> Return this
+// currentHp > 0 ---> Creature still alive with currentHP ---> Return this
 
 /**
  * All creatures can see in bright light.
@@ -113,8 +260,43 @@ function getRemainingHp(maxHp, currentHp, damage) {
  * @returns {boolean} whether the creature can see
  */
 function canSee(light, vision) {
-  // TODO
+  // creature vision && light
+  // bright light = all creatures see ---> Returns true
+  // low-light vision = creatures see in dim light ---> low-light & dark ---> Returns true
+  // darkvision = creatures see in all light conditions ---> dark ---> Returns true
+  // return true / false can creature see?
+
+  if (typeof light !== "string" || typeof vision !== "string") {
+    return "Please enter an approved word for both inputs.";
+  }
+
+  if (light !== "bright" && light !== "dim" && light !== "dark") {
+    return "Invalid input. Please use bright, dim, or dark to describe the light.";
+  }
+
+  if (vision !== "average" && vision !== "low-light" && vision !== "dark") {
+    return "Invalid input. Please use average, low-light, or dark to describe the vision.";
+  }
+
+  let visible = false;
+
+  if (light === "bright") {
+    visible = true;
+  } else if (light === "dim" && vision !== "average") {
+    visible = true;
+  } else if (light === "dark" && vision === "dark") {
+    visible = true;
+  }
+  return visible;
 }
+console.log(canSee("bright", "average")); //true
+console.log(canSee("dim", "average")); //false
+console.log(canSee("dark", "low-light")); //false
+console.log(canSee("dim", "dark")); //true
+console.log(canSee("dark", "dark")); //true
+console.log(canSee("dark", true)); //error
+console.log(canSee("blight", "dark")); //error
+console.log(canSee("bright", "low")); //error
 
 /**
  * A strike deals damage if it hits, unless the strike is a critical hit,
@@ -127,5 +309,34 @@ function canSee(light, vision) {
  * @returns {number} damage dealt by the strike
  */
 function getStrikeDamage(attack, ac, damage) {
-  // TODO
+  // strike hits = true ---> damage delt
+  // strike is critical =  true ---> 2 * damage
+  // strike hits = false ---> damage = 0
+  // use functions above
+  if (
+    typeof attack !== "number" ||
+    typeof ac !== "number" ||
+    typeof damage !== "number"
+  ) {
+    return "Please enter a number for all three inputs.";
+  }
+
+  const NO_HIT = 0;
+  const CRIT_MULTIPLIER = 2;
+
+  let hit = doesStrikeHit(attack, ac);
+  let crit = doesStrikeCrit(attack, ac);
+
+  if (hit === true && crit === true) {
+    return damage * CRIT_MULTIPLIER;
+  } else if (hit) {
+    return damage;
+  } else {
+    return NO_HIT;
+  }
 }
+
+console.log(getStrikeDamage(16, 5, 18)); // 36
+console.log(getStrikeDamage(20, 29, 6)); // 0
+console.log(getStrikeDamage(16, 16, 26)); // 26
+console.log(getStrikeDamage(true, 5, 10)); // error
